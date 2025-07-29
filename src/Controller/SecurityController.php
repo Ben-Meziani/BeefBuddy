19b7ccc8021b45fd195ad4f901f0336144e6250d
meziani <meziani75011@gmail.com>
2025-07-29 03:09:22 +0200

    login correction

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/api/login', name: 'app_login', methods: ['POST'])]
+    public function login(#[CurrentUser] $user = null): Response
+    {
+        return $this->json([
+            'user' => $user ? $user->getId() : null,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // This method can be blank - it will be intercepted by the logout key on your firewall
+    }
+}
