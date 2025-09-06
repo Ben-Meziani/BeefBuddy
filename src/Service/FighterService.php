1dc5693e073ab60b4c70a7e90f8f904c3f7ca669
meziani <meziani75011@gmail.com>
2025-09-06 12:58:12 +0200

    Add cache

--- src/Service/FighterService.php
+++ src/Service/FighterService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Entity\Fighter;
 use App\Entity\Reservation;
+use App\Repository\FighterRepository;
 use Carbon\CarbonImmutable;
 use Carbon\CarbonPeriod;
 use Doctrine\ORM\EntityManagerInterface;
@@ -11,12 +12,16 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class FighterService
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
         private SerializerInterface $serializer,
+        private CacheInterface $cache,
+        private FighterRepository $fighterRepository,
     ) {}
 
     public function index(Request $request)
@@ -25,15 +30,26 @@ class FighterService
         $page = $params['page'] ?? 1;
         $perPage = $params['per_page'] ?? 10;
         $offset = ($page - 1) * $perPage;
-        $totalFighters = $this->entityManager->getRepository(Fighter::class)->count([]);
-        $fighters = $this->entityManager->getRepository(Fighter::class)->findBy([], ['id' => 'DESC'], $perPage, $offset);
+        // $totalFighters = $this->fighterRepository->count([]);
+        $totalFighters = $this->cache->get('total_fighters', function(ItemInterface $item) {
+            $item->expiresAfter(3600);
+            return $this->fighterRepository->count([]);
+        });
+        // $fighters = $this->fighterRepository->findBy([], ['id' => 'DESC'], $perPage, $offset);
+        $fighters = $this->cache->get('fighters', function(ItemInterface $item) use ($perPage, $offset) {
+            $item->expiresAfter(3600);
+            return $this->fighterRepository->findBy([], ['id' => 'DESC'], $perPage, $offset);
+        });
         $fightersArray = array_map(fn(Fighter $fighter) => $fighter->toArray(), $fighters);
         return new JsonResponse(['fighters' => $fightersArray, 'totalFighters' => $totalFighters, 'page' => $page, 'perPage' => $perPage]);
     }
 
     public function show(int $id): JsonResponse
     {
-        $fighter = $this->entityManager->getRepository(Fighter::class)->find($id);
+        $fighter = $this->cache->get('fighter_'.$id, function(ItemInterface $item) use ($id) {
+            $item->expiresAfter(3600);
+            return $this->fighterRepository->find($id);
+        });
 
         if (!$fighter) {
             throw new NotFoundHttpException(sprintf('No fighter found for id "%d"', $id));
@@ -86,5 +102,39 @@ class FighterService
         $this->entityManager->persist($fighter);
         $this->entityManager->flush();
         return new JsonResponse(['message' => 'Fighter registered successfully']);
+    }
+
+    public function update(int $id, Request $request): JsonResponse
+    {
+        // $fighter = $this->fighterRepository->find($id);
+        $fighter = $this->cache->get('fighter_'.$id, function(ItemInterface $item) use ($id) {
+            $item->expiresAfter(3600);
+            return $this->fighterRepository->find($id);
+        });
+        // dd($fighter);
+        if (!$fighter) {
+            throw new NotFoundHttpException(sprintf('No fighter found for id "%d"', $id));
+        }
+        $data = $this->serializer->deserialize($request->getContent(), Fighter::class, 'json');
+        $fighter->setName($data->getName());
+        $fighter->setNickname($data->getNickname());
+        $fighter->setDescription($data->getDescription());
+        $fighter->setWeightClass($data->getWeightClass());
+        $fighter->setWins($data->getWins());
+        $fighter->setLosses($data->getLosses());
+        $fighter->setDraws($data->getDraws());
+        $fighter->setCountry($data->getCountry());
+        $fighter->setBirthday($data->getBirthday());
+        $fighter->setIsActive($data->isActive());
+        $fighter->setHeight($data->getHeight());
+        $fighter->setWeight($data->getWeight());
+        $fighter->setReach($data->getReach());
+        $fighter->setStance($data->getStance());
+        $fighter->setStyle($data->getStyle());
+        $fighter->setPlaceOfBirth($data->getPlaceOfBirth());
+        $fighter->setPricePerTraining($data->getPricePerTraining());
+        $this->cache->delete('fighter_'.$id);
+        $this->cache->delete('fighters');
+        return new JsonResponse(['message' => 'Fighter updated successfully']);
     }
 }
--- src/Service/HomeService.php
+++ src/Service/HomeService.php
@@ -2,19 +2,22 @@
 
 namespace App\Service;
 
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
+// use App\Entity\User;
+use App\Repository\UserRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class HomeService
 {
     public function __construct(
         private JWTEncoderInterface $jwtEncoder,
-        private EntityManagerInterface $entityManager
+        private CacheInterface $cache,
+        private UserRepository $userRepository
     ) {}
-    
+
     public function index(Request $request): JsonResponse
     {
         $jwt = $request->cookies->get('access_token');
@@ -33,7 +36,10 @@ class HomeService
             return new JsonResponse(['error' => 'XSRF token invalide'], 403);
         }
 
-        $user = $this->entityManager->getRepository(User::class)->find($decoded['id']);
+        $user = $this->cache->get('user_'.$decoded['id'], function(ItemInterface $item) use ($decoded) {
+            $item->expiresAfter(30);
+            return $this->userRepository->find($decoded['id']);
+        });
 
         if (!$user) {
             return new JsonResponse(['user' => 'guest'], 200);
